cbd41b8a92d2b6677a79f7d8a4d73d2785ffcbc4
alab139-gif <alab139@hotmail.com>
2026-04-10 00:08:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,25 +6,20 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-bot = Bot(token=TOKEN)
 scheduler = AsyncIOScheduler(timezone="Europe/Lisbon")
 
 async def enviar(mensagem):
+    bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=CHAT_ID, text=mensagem)
 
 async def teste():
     await enviar("🟢 Bot a funcionar! Teste bem sucedido! 🎉")
 
-scheduler.add_job(teste, "cron", hour=0, minute=5)
+scheduler.add_job(teste, "cron", hour=0, minute=10)
 
 async def main():
     scheduler.start()
     print("Bot a correr...")
-    while True:
-        await asyncio.sleep(60)
-
-if __name__ == "__main__":
-    asyncio.run(main())
     while True:
         await asyncio.sleep(60)
 
